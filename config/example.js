67f39efeb0405623112d63f6bb605c57a0d8c949
heyli <heyli@tencent.com>
2018-06-29 15:10:53 +0800

    first commit

---
+++ config/example.js
@@ -0,0 +1,10 @@
+
+const ProxyUrl = '';
+const AppId = '';
+const SecretId = '';
+const SecretKey = '';
+
+exports.ProxyUrl = ProxyUrl;
+exports.AppId = AppId;
+exports.SecretId = SecretId;
+exports.SecretKey = SecretKey;
